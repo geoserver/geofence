a4997af3097e196d1ecdd2e7d54932027e843ceb
etj <etj@geo-solutions.it>
2015-12-17 13:40:00 +0100

    Fix MD5 password hash. Closes #64.

---
+++ src/services/modules/login/impl/src/test/java/org/geoserver/geofence/login/util/MD5UtilTest.java
@@ -0,0 +1,28 @@
+/* (c) 2015 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.geofence.login.util;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ETj (etj at geo-solutions.it)
+ */
+public class MD5UtilTest {
+
+    /**
+     * Test of getHash method, of class MD5Util.
+     */
+    @Test
+    public void testGetHash() {
+        System.out.println("getHash");
+        String password = "test";
+        String expResult = "098f6bcd4621d373cade4e832627b4f6";
+        String result = MD5Util.getHash(password);
+        assertEquals(expResult, result);
+    }
+
+}
